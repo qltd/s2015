b8e0f1e2d63d4beaf4c8732f4dc38fe912152b1d
mjbondra <mjbondra@gmail.com>
2014-07-26 10:06:30 -0400

    create variable file for transition attributes

--- sites/all/themes/s2015/template.php
+++ sites/all/themes/s2015/template.php
@@ -5,3 +5,9 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * s2015 theme.
  */
+
+ function s2015_form_alter(&$form, &$form_state, $form_id) {
+   if ($form_id == 'search_block_form') {
+     $form['actions']['submit']['#value'] = decode_entities('&#xf002;'); // Change the text on the submit button
+   }
+ }

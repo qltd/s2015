106ea5a1dd4f2a435f934601d310298713a9228c
mjbondra <mjbondra@gmail.com>
2014-07-28 17:42:34 -0400

    update search styles and scripts

--- sites/all/themes/s2015/js-src/behaviors/search.js
+++ sites/all/themes/s2015/js-src/behaviors/search.js
@@ -6,17 +6,19 @@
         , submit = $('.site-search .form-submit')
         , text = $('.site-search .form-text');
 
+      text.click(function () {
+        block.addClass('expanded');
+      });
+      text.blur(function () {
+        if (!text.val()) return block.removeClass('expanded');
+      });
+
       submit.click(function (event) {
         event.preventDefault();
         if (text.val()) return $(form).submit();
         block.addClass('expanded');
         text.focus();
       });
-
-      text.blur(function () {
-        if (!text.val()) return block.removeClass('expanded');
-      });
-
     }
   };
 })(jQuery);
--- sites/all/themes/s2015/template.php
+++ sites/all/themes/s2015/template.php
@@ -6,8 +6,11 @@
  * s2015 theme.
  */
 
- function s2015_form_alter(&$form, &$form_state, $form_id) {
-   if ($form_id == 'search_block_form') {
-     $form['actions']['submit']['#value'] = decode_entities('&#xf002;'); // Change the text on the submit button
-   }
- }
+function s2015_form_alter(&$form, &$form_state, $form_id) {
+  if ($form_id == 'search_block_form') {
+    $form['actions']['submit']['#value'] = decode_entities('&#xf002;'); // Change the text on the submit button
+
+    // Alternative (HTML5) placeholder attribute instead of using the javascript
+    $form['search_block_form']['#attributes']['placeholder'] = t('Type Keyword');
+  }
+}

26512f47fb9c7c39566034ee939a45f264449708
mjbondra <mjbondra@gmail.com>
2014-08-03 10:43:30 -0400

    update styles

--- sites/all/themes/s2015/templates/system/page.tpl.php
+++ sites/all/themes/s2015/templates/system/page.tpl.php
@@ -101,7 +101,7 @@
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if ($title): ?>
-        <h1><?php print $title; ?></h1>
+        <h1 class="content-title"><?php print $title; ?></h1>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
       <?php print $messages; ?>

d411cd3eebe73cdc0c8378d8aad4e054d336cc3a
mjbondra <mjbondra@gmail.com>
2014-08-03 00:22:52 -0400

    add styles for

--- sites/all/themes/s2015/js-src/behaviors/navigation-primary-sub.js
+++ sites/all/themes/s2015/js-src/behaviors/navigation-primary-sub.js
@@ -2,6 +2,37 @@
   Drupal.behaviors.s2015PrimarySubNavigation = {
     attach: function () {
 
+      /**
+       * For Attendees Sub-Navigation
+       */
+      var menu = $('.primary-sub-navigation-for-attendees');
+      if (!menu.length) return;
+      var col3 = $('.primary-navigation-attendees-col-3')
+        , informationPlanning = $('.primary-navigation-information-planning')
+        , programsEvents = $('.primary-navigation-programs-events');
+
+      programsEvents.click(function () {
+        if (programsEvents.hasClass('active-trail')) {
+          programsEvents.removeClass('active-trail').parent().removeClass('active-trail');
+          col3.removeClass('active-trail').parent().removeClass('active-trail');
+        } else {
+          programsEvents.addClass('active-trail').parent().addClass('active-trail');
+          col3.addClass('active-trail').parent().addClass('active-trail');
+        }
+      });
+      informationPlanning.click(function () {
+        informationPlanning.toggleClass('active-trail');
+      });
+
+      // sync active state across related "For Attendees" columns
+      if (programsEvents.hasClass('active-trail')) col3
+        .addClass('active-trail')
+        .parent()
+        .addClass('active-trail');
+      if (col3.hasClass('active-trail')) programsEvents
+        .addClass('active-trail')
+        .parent()
+        .addClass('active-trail');
     }
   };
 })(jQuery);

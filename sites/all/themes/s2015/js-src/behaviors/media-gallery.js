0a176b0bd3e31e57f50b2a2a45897c8720de2ec5
amersm <almayer2307@gmail.com>
2014-11-20 15:34:24 -0500

    finalize media gallery js

--- sites/all/themes/s2015/js-src/behaviors/media-gallery.js
+++ sites/all/themes/s2015/js-src/behaviors/media-gallery.js
@@ -1,24 +1,22 @@
 (function ($) {
   Drupal.behaviors.s2015MediaGallery = {
     attach: function () {
-      var item = $('.gallery-media-thumb');
-      if (!item.length) return;
+      var thumbs = $('.gallery-media-thumb')
+        , gallery = $('.gallery-media');
+      if (!thumbs.length || !gallery.length) return;
 
-      item.click(function (e) {
-          e.preventDefault();
-          $('.gallery-media-thumb').removeClass('active-media');
-          $(this).addClass('active-media');
-          if ($('.media-gallery-lg-image').length) {
-            var content = $(this).html();
-            console.log(content);
-            $('.gallery-media').html(content);
-          } else if ($('.button-play-video').length) {
-            var oldElem = $('.button-play-video');
-            var newElemUrl = oldElem.getAttribute('data-embed-url');
-            console.log(newElemUrl);
-            //$('.gallery-media').html('<iframe width="570" height="363" src="'+embedUrl+'" frameborder="0" allowfullscreen></iframe>');
-
-          }
+      thumbs.click(function (e) {
+        e.preventDefault();
+        var mediaActive = $(this);
+        thumbs.removeClass('active-media');
+        mediaActive.addClass('active-media');
+        if (mediaActive.children('.media-gallery-lg-image').length) {
+          var content = mediaActive.html();
+          gallery.html(content);
+        } else if (mediaActive.children('.button-play-video').length) {
+          var embedUrl = $('.button-play-video').attr('data-embed-url');
+          gallery.html('<iframe width="570" height="363" src="' + embedUrl + '" frameborder="0" allowfullscreen></iframe>');
+        }
       });
     }
   };
